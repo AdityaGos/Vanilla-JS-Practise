863442078069760e3104c961f440463aacf6f3f7
Aditya Goswami <adityagoswami071@gmail.com>
2024-02-12 00:01:26 +0530

    feart: rendering products from API

--- ProductListing/src/index.js
+++ ProductListing/src/index.js
@@ -1,44 +1,58 @@
 import { getProducts } from "./util.js"
 
 let productData=[]
-const getAllProducts = async () => {
-    try {
-      // Call getProducts to retrieve products
-      const products = await getProducts();
+// const getAllProducts = async () => {
+//     try {
+//       // Call getProducts to retrieve products
+//       const products = await getProducts();
   
-      // Log all products
-      productData = JSON.parse(JSON.stringify(products.products))
-      console.log(productData)
-    } catch (error) {
-      console.error('Error fetching products:', error);
-    }
-  };
-  getAllProducts()
-document.addEventListener('DOMContentLoaded',()=>{
+//       // Log all products
+//       productData = JSON.parse(JSON.stringify(products.products))
+//       console.log(productData)
+//     } catch (error) {
+//       console.error('Error fetching products:', error);
+//     }
+//   };
 
+document.addEventListener('DOMContentLoaded',()=>{
+    
     const productContainerRef = document.querySelector('.product-container')
     
-    console.log('hello')
-
-    const renderProductList = (id, title, image, price) => {
+    const productHTML = (id, title, image, price) => {
         return `
         <div class="product-container">
             <div class="product" data-product-id=${id}>
-            <img src="${image}" alt="" class="Product-image">
-                <span>${title}</span>
-                <span> $ ${price}</span>
+            <img src="${image}" alt="" class="product--image">
+            <div class="product-info">
+            <span class="product--title">${title}</span>
+            <span class="product--price"> $ ${price}</span>
+            </div>
+               
             </div>
         </div>
         `
       }
-   
-        for (const product of productData) {
-            console.log('inside for loop')
-            // console.log(product)
-            // console.log(product)
-        //   const html = renderProductList(movie.title, movie.poster, movie.year);
-        //   productContainerRef.insertAdjacentHTML("beforeend", html);
+  
+     const renderProducts = (products) =>{
+        const productArr = products.products
+        for (const product of productArr) {
+            const html = productHTML(product.id,product.title, product.thumbnail, product.price, );
+            productContainerRef.insertAdjacentHTML("beforeend", html);
         }
+        console.log(productArr)
+     }
+     (async () => {
+        try {
+            // Fetch product data
+            const products = await getProducts();
+            // Render products
+            renderProducts(products);
+        } catch (error) {
+            console.error('Error rendering products:', error);
+        }
+    })()
+        
+    
     
         
     
--- ProductListing/src/util.js
+++ ProductListing/src/util.js
@@ -1,7 +1,7 @@
  export const getProducts =async (page=1 , skip)=> { 
 
   try{
-    const response = await fetch('https://dummyjson.com/products')
+    const response = await fetch('https://dummyjson.com/products?limit=7')
     const result = await response.json()
     return result
 
