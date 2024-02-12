f10bb3a6e5fa5edcbf5b88325c3a60897f70c549
Aditya Goswami <adityagoswami071@gmail.com>
2024-02-13 00:38:05 +0530

    feat: flatArray polyfill created

---
+++ Polyfill/flatArray.js
@@ -0,0 +1,30 @@
+const arr = [1,2,3,[[4,5]]]
+// console.log(arr.flat(3))
+
+
+Array.prototype.myflat = function(depth)
+{
+    let res= []
+    // this has access of the array
+    if(!Array.isArray(this))
+    {
+        throw new Error(`${this}.flat is not a function`)
+    }
+    this.forEach((elem)=>{
+
+        if(Array.isArray(elem) && depth>0)
+        {
+            // recursively calling myflat method 
+            // if the element is a nested array and pushing the element using spread 
+            // decreasing the depth
+            res.push(...elem.myflat(depth-1))
+        }
+        else{
+            // if the elem is not an array then just push the element into res arr
+            res.push(elem)
+        }
+    })
+    return res
+}
+
+console.log(arr.myflat(5))
