03fa4edb973c0e99261602e086ec7fcf33f6b4f4
Aditya Goswami <adityagoswami071@gmail.com>
2024-02-18 23:49:02 +0530

    polyfill comment

--- Polyfill/flatArray.js
+++ Polyfill/flatArray.js
@@ -17,6 +17,7 @@ Array.prototype.myflat = function(depth)
             // recursively calling myflat method 
             // if the element is a nested array and pushing the element using spread 
             // decreasing the depth
+            // we got res = [4,5] then we ... spread it and push to main res array
             res.push(...elem.myflat(depth-1))
         }
         else{
