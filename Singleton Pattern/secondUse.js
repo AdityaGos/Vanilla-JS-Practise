c7a84e8fa79d23b23b7380a023e8161f8352556a
Aditya Goswami <adityagoswami071@gmail.com>
2024-02-20 14:49:19 +0530

    Shifted Design pattern directory to different folder

--- Singleton Pattern/secondUse.js
+++
@@ -1,9 +0,0 @@
-import logger from "./fancyLogger.js";
-
-
-export default function logSecondImplementation()
-{
-    logger.printLogCount()
-    logger.log('Second File')
-    logger.printLogCount()
-}
